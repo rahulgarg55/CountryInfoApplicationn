bf7deafedb56eaa53a1c0e5ba48a0f7349f4d59f
rahulgarg55 <gargr010>
2024-08-30 20:47:42 +0530

    added-swagger

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const countriesRoutes = require('./routes/countries');
 
 const app = express();
@@ -9,8 +11,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/api/countries', countriesRoutes);
+
+// Root route to redirect to API documentation
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`API documentation available at http://localhost:${PORT}/api-docs`);
 });
