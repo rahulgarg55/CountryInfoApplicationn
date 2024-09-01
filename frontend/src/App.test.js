07c38aca07c55d75edac2f714072bd320915271b
rahulgarg55 <gargr010>
2024-09-01 13:49:47 +0530

    documentation

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,7 @@
+/**
+ * Tests the rendering of the main App component.
+ * Verifies that the "learn react" text is present in the rendered output.
+ */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
