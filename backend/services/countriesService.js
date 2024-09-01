228cef74274ed7c86662e84c3ac33d8e9ae1f7e5
rahulgarg55 <gargr010>
2024-09-01 13:51:44 +0530

    documentation

--- backend/services/countriesService.js
+++ backend/services/countriesService.js
@@ -1,3 +1,25 @@
+/**
+ * Provides functions to search for countries by name and retrieve country details by country code.
+ *
+ * @module countriesService
+ */
+
+/**
+ * Searches for countries by name.
+ *
+ * @param {string} name - The name of the country to search for.
+ * @returns {Promise<Object[]>} - An array of country objects matching the search query.
+ */
+
+
+/**
+ * Retrieves detailed information about a country by its country code.
+ *
+ * @param {string} countryCode - The ISO 3166-1 alpha-3 country code.
+ * @returns {Promise<Object>} - The country object with detailed information.
+ * @throws {Error} - Throws an error if the country is not found.
+ */
+
 const axios = require('axios');
 
 const API_BASE_URL = 'https://restcountries.com/v3.1';
